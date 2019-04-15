b2b6b9b62a78434d19fc11d7e722712f162dbab7
Daniel Lee <dantheman6100@gmail.com>
2019-04-15 14:49:46 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Using deep learning to search for images of gene expression in chicken embryos',
+    author='Daniel Lee',
+    license='MIT',
+)
